2c0ff6fbebf00021f3b3034f108d91ed280874b8
Dan-Lucian Nita <dan-luciannita@Dan-Lucians-MacBook-Air.local>
2022-12-20 20:10:33 +0200

    fix: File renamings

--- PetCareSwiftApp/LoginView.swift
+++ PetCareSwiftApp/LoginView.swift
@@ -62,7 +62,7 @@ struct AppContentView: View {
     var body: some View {
         return Group {
             if signInSuccess {
-                AllPetsView()
+                AppHomeView()
             }
             else {
                 LoginView(signInSuccess: $signInSuccess)
