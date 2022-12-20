51e9ad433e3c36022e38c0aa884c035b3091eb5d
Dan-Lucian Nita <dan-luciannita@Dan-Lucians-MacBook-Air.local>
2022-12-20 18:20:08 +0200

    feat: First commint

---
+++ PetCareSwiftApp/PetCareSwiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetCareSwiftAppApp.swift
+//  PetCareSwiftApp
+//
+//  Created by Dan-Lucian Nita on 20.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct PetCareSwiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
